4b9713fb2c27443e437851dca0f9cfcffb8b6063
klballard <kyleleeballard@gmail.com>
2020-10-01 16:21:26 -0400

    101update

---
+++ nasa_api/script.js
@@ -0,0 +1,34 @@
+const baseURL = 'https://api.nasa.gov/planetary/earth/assets';
+const key = 'FMgw6baYfiNbmCa5prGwoNG0c1wcWR84qXSygfIr';
+let url;
+
+// search form field
+const searchForm = document.querySelector('form');
+const submitBtn = document.querySelector('.submit');
+const latitude = document.querySelector('.latitude');
+const longitude = document.querySelector('.longitude');
+const enterDate = document.querySelector('.enter-date');
+
+searchForm.addEventListener('submit', fetchResults);
+
+//fetching from the api
+function fetchResults(e) {
+    e.preventDefault();
+    url = baseURL + '?lon=' + longitude.value + '&lat=' + latitude.value + '&date=' + enterDate.value + '&&dim=0.4&api_key=' + key;
+    fetch(url)
+        .then(function(result) {
+            return result.json();
+        }).then(function(json) {
+            displayResults(json);
+            console.log(json);
+        });
+}
+
+function displayResults(json) {
+    console.log("DisplayResults", json);
+}
+
+function displayResults(json) {
+    document.getElementById('result').src = json.url;
+
+}
